1c09c92bd179e5ac4102e49f3080f22f4c1bb272
Whatsupyuan <hryuan911@qq.com>
2018-01-24 18:29:50 +0800

    add bullet and shooting

--- script/useBullet/alien_invasion001_useBullet.py
+++ script/useBullet/alien_invasion001_useBullet.py
@@ -21,6 +21,13 @@ def run_game():
         gf.check_events_keepMove(ship,setting,bullets,screen)
         ship.update()
         bullets.update()
+        # 删除已经跑出屏幕顶部字段,降低程序运行消耗
+        for bullet in bullets.copy():
+            if bullet.rect.bottom <= 0:
+                bullets.remove(bullet)
+        # 打印当前在 Group - Bullets 中的有效弹药
+        # print(len(bullets))
         gf.update_screen(setting , screen , ship , bullets)
 
+# 运行
 run_game()
