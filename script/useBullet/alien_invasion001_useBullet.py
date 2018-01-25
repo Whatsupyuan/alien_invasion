db8d4a6ba823bc8bf56a1c92c47d9b8c13e40338
Whatsupyuan <hryuan911@qq.com>
2018-01-25 11:43:58 +0800

    add code

--- script/useBullet/alien_invasion001_useBullet.py
+++ script/useBullet/alien_invasion001_useBullet.py
@@ -7,9 +7,7 @@ from pygame.sprite import Group
 setting = Setting()
 
 def run_game():
-    pygame.init()
-    screen = pygame.display.set_mode((setting.width,setting.height))
-    pygame.display.set_caption("Alien Invasion")
+    screen = init_pygame()
     # 创建飞船
     ship = Ship(screen , setting)
     # 存储子弹的编组
@@ -24,6 +22,13 @@ def run_game():
         # 打印当前在 Group - Bullets 中的有效弹药
         # print(len(bullets))
         gf.update_screen(setting , screen , ship , bullets)
+
+# 重构初始化 pygame
+def init_pygame():
+    pygame.init()
+    screen = pygame.display.set_mode((setting.width, setting.height))
+    pygame.display.set_caption("Alien Invasion")
+    return screen
 
 # 运行
 run_game()
